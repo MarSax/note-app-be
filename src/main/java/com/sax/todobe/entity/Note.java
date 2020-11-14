0231010f00ed90054e605bb473bf5a7e428e5e80
MarSax <msassanelli12@gmail.com>
2020-11-14 21:09:25 +0100

    added web security config

--- src/main/java/com/sax/todobe/entity/Note.java
+++ src/main/java/com/sax/todobe/entity/Note.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Data
 public class Note {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="seq_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
